b7939384b57dcbf21d17de39181298857c1a2b4d
Danielle Knudson <knudson.danielle@gmail.com>
2015-08-08 22:22:31 -0700

    Add new toy problem primeEvenMultiple

---
+++ primeEvenMultiple/primeEvenMultiple.js
@@ -0,0 +1,9 @@
+// TOY PROBLEM FROM codewars.com
+// AUTHORED BY sohilpandya (codewars.com/users/sohilpandya)
+
+// Create a function which checks a number for three different properties.
+
+// is the number prime?
+// is the number even?
+// is the number a multiple of 10?
+// Each should return either true or false, which should be given as an array.
